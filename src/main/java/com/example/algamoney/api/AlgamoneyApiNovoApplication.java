27f890bb4f7dbd6841c5ac7d4e7977ba4bf88e9e
Lucas Moraes <lucasmoraesostemberg761@gmail.com>
2024-07-03 12:26:23 -0400

    Commit do Modulo de Segurança HttpBasic

---
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiNovoApplication.java
@@ -0,0 +1,13 @@
+package com.example.algamoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApiNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiNovoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -0,0 +1,67 @@
+package com.example.algamoney.api.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	// ctrl + 3 => acessar comandos 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	private String nome;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+
+	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	;
+	
+	
+	
+}
